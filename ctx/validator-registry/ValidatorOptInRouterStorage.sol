fda8587a461c707609c91a84dd87cf128f987f2d
Shawn <44221603+shaspitz@users.noreply.github.com>
2025-02-18 19:17:22 -0800

    contracts for cursor ctx

---
+++ ctx/validator-registry/ValidatorOptInRouterStorage.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: BSL 1.1
+pragma solidity 0.8.26;
+
+import {IVanillaRegistry} from "../interfaces/IVanillaRegistry.sol";
+import {IMevCommitAVS} from "../interfaces/IMevCommitAVS.sol";
+import {IMevCommitMiddleware} from "../interfaces/IMevCommitMiddleware.sol";
+
+/// @title ValidatorOptInRouterStorage
+/// @notice Storage components of the ValidatorOptInRouter contract.
+contract ValidatorOptInRouterStorage {
+
+    /// @notice The address of the vanilla registry contract.
+    IVanillaRegistry public vanillaRegistry;
+
+    /// @notice The address of the mev-commit AVS contract.
+    IMevCommitAVS public mevCommitAVS;
+
+    /// @notice The address of the mev-commit middleware contract.
+    IMevCommitMiddleware public mevCommitMiddleware;
+
+    /// @dev See https://docs.openzeppelin.com/upgrades-plugins/1.x/writing-upgradeable#storage-gaps
+    uint256[48] private __gap;
+}
