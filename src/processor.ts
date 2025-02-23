ea814c7f2c53cfa942ef86a3d3fadf418b07a059
Shawn <44221603+shaspitz@users.noreply.github.com>
2025-02-22 16:07:30 -0800

    Update processor.ts

--- src/processor.ts
+++ src/processor.ts
@@ -1,5 +1,6 @@
 import { initVanillaRegistryProcessor } from './vanillaRegistryProcessor.js'
 import { initMevCommitAVSProcessor } from './mevCommitAVSProcessor.js'
+import { initMevCommitMiddlewareProcessor } from './mevCommitMiddlewareProcessor.js'
 import { EthChainId } from '@sentio/sdk/eth'
 
 const vanillaRegistryAddress = '0x47afdcB2B089C16CEe354811EA1Bbe0DB7c335E9'
@@ -23,5 +24,17 @@ initMevCommitAVSProcessor(
 const mevCommitAVSHoleskyAddress = '0xEDEDB8ed37A43Fd399108A44646B85b780D85DD4'
 initMevCommitAVSProcessor(
   mevCommitAVSHoleskyAddress,
+  EthChainId.HOLESKY,
+)
+
+const mevCommitMiddlewareAddress = '0x21fD239311B050bbeE7F32850d99ADc224761382'
+initMevCommitMiddlewareProcessor(
+  mevCommitMiddlewareAddress,
+  EthChainId.ETHEREUM,
+)
+
+const mevCommitMiddlewareHoleskyAddress = '0x79FeCD427e5A3e5f1a40895A0AC20A6a50C95393'
+initMevCommitMiddlewareProcessor(
+  mevCommitMiddlewareHoleskyAddress,
   EthChainId.HOLESKY,
 )
